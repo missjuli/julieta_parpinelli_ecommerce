5dd6b007657748e6fb3db6df53e51742b6980d25
Julieta Parpinelli <julietaparpinelli@gmail.com>
2022-03-02 20:55:15 -0300

    Adding navbar & title

---
+++ src/components/logo.js
@@ -0,0 +1,7 @@
+function Logo() {
+    return(
+        <img src="./assets/logo.png" alt="logo"/>
+    )
+}
+
+export default Logo
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import Style from './style.css'
+//import Style from './style.css'
+import 'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap'; 
 
-ReactDOM.render( < App / > , document.getElementById('root'))
+ReactDOM.render( < App /> , document.getElementById('root'))
