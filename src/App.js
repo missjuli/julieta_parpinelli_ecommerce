1ea6de05f8ac5f9c3872ca593b134bd2dc0a5359
Julieta Parpinelli <julietaparpinelli@gmail.com>
2022-03-23 20:47:03 -0300

    tenth task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar"
 import Main from "./components/Main"
 import { BrowserRouter } from "react-router-dom"
+import { context } from "./CartContext"
 
 function App() {
     return(
